974ea553ac34f5aa5bd1f2041716e996329a48c0
Nagaveni.Barker <nagavenibarker6@gmail.com>
2025-11-27 15:35:26 +0530

    Update elec.py

--- elec.py
+++ elec.py
@@ -1,10 +1,16 @@
 import sys
+
 RATE_PER_UNIT = 5  # ₹5 per unit
+
 if len(sys.argv) == 2:
     print("User provided units:")
     units = float(sys.argv[1])
 else:
     print("No input given -- using default units:")
     units = 100   # default units
+
 bill = units * RATE_PER_UNIT
-print("Units Consume
+
+print("Units Consumed:", units)
+print("Rate Per Unit: ₹", RATE_PER_UNIT)
+print("Total Electricity Bill: ₹", bill)
